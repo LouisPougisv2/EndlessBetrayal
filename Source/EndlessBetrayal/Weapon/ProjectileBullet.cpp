51b6d25c20b96f802ad0c4a410de3720e5122c01
Louis <louis.pougis@gmail.com>
2024-01-17 16:13:35 +0100

    Predict Projectile Path + Post Edit Change Property override for Editor bug fix

--- Source/EndlessBetrayal/Weapon/ProjectileBullet.cpp
+++ Source/EndlessBetrayal/Weapon/ProjectileBullet.cpp
@@ -14,6 +14,47 @@ AProjectileBullet::AProjectileBullet()
 	//Following line makes sure that the projectile will keep its rotation aligned with the velocity
 	ProjectileMovementComponent->bRotationFollowsVelocity = true;
 	ProjectileMovementComponent->SetIsReplicated(true);
+	ProjectileMovementComponent->InitialSpeed = InitialSpeed;
+	ProjectileMovementComponent->MaxSpeed = InitialSpeed;
+}
+
+#if WITH_EDITOR
+void AProjectileBullet::PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent)
+{
+	Super::PostEditChangeProperty(PropertyChangedEvent);
+
+	if(PropertyChangedEvent.GetPropertyName() == GET_MEMBER_NAME_CHECKED(AProjectileBullet, InitialSpeed))
+	{
+		if(IsValid(ProjectileMovementComponent))
+		{
+			ProjectileMovementComponent->InitialSpeed = InitialSpeed;
+			ProjectileMovementComponent->MaxSpeed = InitialSpeed;
+		}
+	}
+}
+#endif
+
+void AProjectileBullet::BeginPlay()
+{
+	Super::BeginPlay();
+
+	FPredictProjectilePathParams PathParams;
+	//PredictProjectilePathParams.bTraceComplex -> if true, traces against the mesh itself instead of collisions or assets
+	PathParams.bTraceWithChannel = true;
+	PathParams.bTraceWithCollision = true;	//Allows us to predict a projectile path and generate hit events as we're tracing against the collision object in our world
+	PathParams.DrawDebugTime = 5.0f;	//TODO : To remove
+	PathParams.DrawDebugType = EDrawDebugTrace::ForDuration;	//TODO : To remove
+	PathParams.LaunchVelocity = GetActorForwardVector() * InitialSpeed;
+	PathParams.MaxSimTime = 4.0f; //Amount of time the projectile would be flying through the air
+	PathParams.ProjectileRadius = 5.0f;
+	PathParams.SimFrequency = 20.0f;
+	PathParams.StartLocation = GetActorLocation();
+	PathParams.TraceChannel = ECC_Visibility;
+	PathParams.ActorsToIgnore.Add(this);
+	
+	FPredictProjectilePathResult PredictProjectilePathResult;
+	
+	UGameplayStatics::PredictProjectilePath(this, PathParams, PredictProjectilePathResult);
 }
 
 void AProjectileBullet::OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
